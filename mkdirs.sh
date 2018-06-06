042d65f83334d712cb8e77964caa72f72123e82a
abhijeetchopra <abhijeetchopra@yahoo.com>
2018-06-05 23:42:56 -0400

    Updated

--- mkdirs.sh
+++ mkdirs.sh
@@ -5,7 +5,14 @@
 # Desc: Script to populate dummy dirs with files
 # --------------------------------------
 
-touch file_0{1..9}
+# creating dummy files
+touch file_0{1..9} 
+
+# creating dummy directories
 mkdir dir_0{1..9}
+
+# copying all dummy files to all directories 
 echo dir_0{1..9} | xargs -n1 cp file_0{1..9}
+
+# deleting dummy files in parent directory
 rm file_0{1..9}
